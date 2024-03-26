acf4f76522d1852463d96f6ff654cfdbef5f4259
Antonio Nunez <antonio@altresear.ch>
2024-03-26 13:47:50 +0800

    feat: update scripts-altlayer

--- scripts-altlayer/checkRoles.ts
+++ scripts-altlayer/checkRoles.ts
@@ -1,45 +1,57 @@
 import { ethers } from 'ethers'
 import UpgradeExecutor from '@arbitrum/nitro-contracts/build/contracts/src/mocks/UpgradeExecutorMock.sol/UpgradeExecutorMock.json'
 import { getSigner } from './erc20TokenBridgeDeployment'
-import { getExecutorAddress } from './getExecutorAddress'
+import { getExecutorAddress } from './getExecutorAddressImpl'
 
 async function main() {
   // Read the environment variables
-  const privateKey = process.env.PRIVATE_KEY
-  const TOKEN_BRIDGE_CREATOR = process.env.TOKEN_BRIDGE_CREATOR || ''
+  const PRIVATE_KEY = process.env.PRIVATE_KEY
   const L2_RPC_URL = process.env.L2_RPC_URL
   const L3_RPC_URL = process.env.L3_RPC_URL
-  const OPERATOR = process.env.OPERATOR
+  // Provide either TOKEN_BRIDGE_CREATOR and INBOX, or just UPGRADE_EXECUTOR
+  const TOKEN_BRIDGE_CREATOR = process.env.TOKEN_BRIDGE_CREATOR || ''
   const INBOX = process.env.INBOX || ''
-  if (!privateKey || !L3_RPC_URL || !OPERATOR) {
+  const UPGRADE_EXECUTOR_PROXY = process.env.UPGRADE_EXECUTOR_PROXY
+  const OPERATOR = process.env.OPERATOR
+  if (!PRIVATE_KEY || !L2_RPC_URL || !L3_RPC_URL || !OPERATOR) {
     throw new Error('Required environment variable not found')
   }
 
   // Generating providers from RPCs
   const L2Provider = new ethers.providers.JsonRpcProvider(L2_RPC_URL)
   const L3Provider = new ethers.providers.JsonRpcProvider(L3_RPC_URL)
-  const l3Deployer = getSigner(L3Provider, privateKey)
+  const l3Deployer = getSigner(L3Provider, PRIVATE_KEY)
 
-  const executorContractAddress = await getExecutorAddress(
-    TOKEN_BRIDGE_CREATOR,
-    INBOX,
-    L2Provider,
-    L3Provider
-  )
+  let executorContractAddress = ''
+  if (!UPGRADE_EXECUTOR_PROXY) {
+    console.log(`UPGRADE_EXECUTOR_PROXY is not provided, getting address...`)
+    executorContractAddress = await getExecutorAddress(
+      TOKEN_BRIDGE_CREATOR,
+      INBOX,
+      L2Provider,
+      L3Provider
+    )
+  } else {
+    console.log(`UPGRADE_EXECUTOR_PROXY is provided`)
+    executorContractAddress = UPGRADE_EXECUTOR_PROXY
+  }
+  console.log('executor address:', executorContractAddress)
 
-  console.log('executor address: ', executorContractAddress)
-  //Defining upgrade executor contract
+  // Defining upgrade executor contract
   const executorContract__factory = new ethers.Contract(
     executorContractAddress,
     UpgradeExecutor.abi,
     l3Deployer
   )
   const upgradeExecutor = executorContract__factory.connect(l3Deployer)
-  // const adminRole = await upgradeExecutor.ADMIN_ROLE()
-  const executorRole = await upgradeExecutor.EXECUTOR_ROLE()
 
+  const executorRole = await upgradeExecutor.EXECUTOR_ROLE()
   const hasExecutorRole = await upgradeExecutor.hasRole(executorRole, OPERATOR)
-  console.log('has executor role: ', hasExecutorRole)
+  console.log('has executor role:', hasExecutorRole)
+
+  const adminRole = await upgradeExecutor.ADMIN_ROLE()
+  const hasAdminRole = await upgradeExecutor.hasRole(adminRole, OPERATOR)
+  console.log('has admin role:', hasAdminRole)
 }
 
 // Run the script
--- scripts-altlayer/getExecutorAddressImpl.ts
+++ scripts-altlayer/getExecutorAddressImpl.ts
@@ -19,13 +19,13 @@ export async function getExecutorAddress(
   const upgradeExecutorProxy = (
     await l1TokenBridgeCreator.inboxToL2Deployment(inbox)
   ).upgradeExecutor
-  console.log('executor proxy: ', upgradeExecutorProxy)
+  console.log('executor proxy:', upgradeExecutorProxy)
 
   let upgradeExecutor = await l3Provider.getStorageAt(
     upgradeExecutorProxy,
     '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc'
   )
   upgradeExecutor = upgradeExecutor.replace(/^(0x)0+((\w{4})+)$/, '$1$2')
-  console.log('executor: ', upgradeExecutor)
+  console.log('executor:', upgradeExecutor)
   return upgradeExecutorProxy
 }
